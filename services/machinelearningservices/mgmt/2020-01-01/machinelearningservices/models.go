5e5994829205e98df768b494b4233bb4a9718ea0
SDK Automation <sdkautomation@microsoft.com>
2020-02-25 13:15:36 +0000

    Generated from e1521f50a9573e55038661c6f206d55ddd0430d3
    Adding user identities

--- services/machinelearningservices/mgmt/2020-01-01/machinelearningservices/models.go
+++ services/machinelearningservices/mgmt/2020-01-01/machinelearningservices/models.go
@@ -284,13 +284,19 @@ func PossibleRemoteLoginPortPublicAccessValues() []RemoteLoginPortPublicAccess {
 type ResourceIdentityType string
 
 const (
+	// None ...
+	None ResourceIdentityType = "None"
 	// SystemAssigned ...
 	SystemAssigned ResourceIdentityType = "SystemAssigned"
+	// SystemAssignedUserAssigned ...
+	SystemAssignedUserAssigned ResourceIdentityType = "SystemAssigned, UserAssigned"
+	// UserAssigned ...
+	UserAssigned ResourceIdentityType = "UserAssigned"
 )
 
 // PossibleResourceIdentityTypeValues returns an array of possible values for the ResourceIdentityType const type.
 func PossibleResourceIdentityTypeValues() []ResourceIdentityType {
-	return []ResourceIdentityType{SystemAssigned}
+	return []ResourceIdentityType{None, SystemAssigned, SystemAssignedUserAssigned, UserAssigned}
 }
 
 // Status enumerates the values for status.
@@ -1025,7 +1031,7 @@ type ComputeResource struct {
 	ID *string `json:"id,omitempty"`
 	// Name - READ-ONLY; Specifies the name of the resource.
 	Name *string `json:"name,omitempty"`
-	// Identity - READ-ONLY; The identity of the resource.
+	// Identity - The identity of the resource.
 	Identity *Identity `json:"identity,omitempty"`
 	// Location - Specifies the location of the resource.
 	Location *string `json:"location,omitempty"`
@@ -1041,6 +1047,9 @@ type ComputeResource struct {
 func (cr ComputeResource) MarshalJSON() ([]byte, error) {
 	objectMap := make(map[string]interface{})
 	objectMap["properties"] = cr.Properties
+	if cr.Identity != nil {
+		objectMap["identity"] = cr.Identity
+	}
 	if cr.Location != nil {
 		objectMap["location"] = cr.Location
 	}
@@ -1713,8 +1722,30 @@ type Identity struct {
 	PrincipalID *string `json:"principalId,omitempty"`
 	// TenantID - READ-ONLY; The tenant ID of resource.
 	TenantID *string `json:"tenantId,omitempty"`
-	// Type - The identity type. Possible values include: 'SystemAssigned'
+	// Type - The identity type. Possible values include: 'SystemAssigned', 'UserAssigned', 'SystemAssignedUserAssigned', 'None'
 	Type ResourceIdentityType `json:"type,omitempty"`
+	// UserAssignedIdentities - The list of user identities associated with resource. The user identity dictionary key references will be ARM resource ids in the form: '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.ManagedIdentity/userAssignedIdentities/{identityName}'.
+	UserAssignedIdentities map[string]*IdentityUserAssignedIdentitiesValue `json:"userAssignedIdentities"`
+}
+
+// MarshalJSON is the custom marshaler for Identity.
+func (i Identity) MarshalJSON() ([]byte, error) {
+	objectMap := make(map[string]interface{})
+	if i.Type != "" {
+		objectMap["type"] = i.Type
+	}
+	if i.UserAssignedIdentities != nil {
+		objectMap["userAssignedIdentities"] = i.UserAssignedIdentities
+	}
+	return json.Marshal(objectMap)
+}
+
+// IdentityUserAssignedIdentitiesValue ...
+type IdentityUserAssignedIdentitiesValue struct {
+	// PrincipalID - READ-ONLY; The principal id of user assigned identity.
+	PrincipalID *string `json:"principalId,omitempty"`
+	// ClientID - READ-ONLY; The client id of user assigned identity.
+	ClientID *string `json:"clientId,omitempty"`
 }
 
 // KeyVaultProperties ...
@@ -2472,7 +2503,7 @@ type PrivateEndpointConnection struct {
 	ID *string `json:"id,omitempty"`
 	// Name - READ-ONLY; Specifies the name of the resource.
 	Name *string `json:"name,omitempty"`
-	// Identity - READ-ONLY; The identity of the resource.
+	// Identity - The identity of the resource.
 	Identity *Identity `json:"identity,omitempty"`
 	// Location - Specifies the location of the resource.
 	Location *string `json:"location,omitempty"`
@@ -2489,6 +2520,9 @@ func (pec PrivateEndpointConnection) MarshalJSON() ([]byte, error) {
 	objectMap := make(map[string]interface{})
 	if pec.PrivateEndpointConnectionProperties != nil {
 		objectMap["properties"] = pec.PrivateEndpointConnectionProperties
+	}
+	if pec.Identity != nil {
+		objectMap["identity"] = pec.Identity
 	}
 	if pec.Location != nil {
 		objectMap["location"] = pec.Location
@@ -2607,7 +2641,7 @@ type PrivateLinkResource struct {
 	ID *string `json:"id,omitempty"`
 	// Name - READ-ONLY; Specifies the name of the resource.
 	Name *string `json:"name,omitempty"`
-	// Identity - READ-ONLY; The identity of the resource.
+	// Identity - The identity of the resource.
 	Identity *Identity `json:"identity,omitempty"`
 	// Location - Specifies the location of the resource.
 	Location *string `json:"location,omitempty"`
@@ -2624,6 +2658,9 @@ func (plr PrivateLinkResource) MarshalJSON() ([]byte, error) {
 	objectMap := make(map[string]interface{})
 	if plr.PrivateLinkResourceProperties != nil {
 		objectMap["properties"] = plr.PrivateLinkResourceProperties
+	}
+	if plr.Identity != nil {
+		objectMap["identity"] = plr.Identity
 	}
 	if plr.Location != nil {
 		objectMap["location"] = plr.Location
@@ -2785,7 +2822,7 @@ type Resource struct {
 	ID *string `json:"id,omitempty"`
 	// Name - READ-ONLY; Specifies the name of the resource.
 	Name *string `json:"name,omitempty"`
-	// Identity - READ-ONLY; The identity of the resource.
+	// Identity - The identity of the resource.
 	Identity *Identity `json:"identity,omitempty"`
 	// Location - Specifies the location of the resource.
 	Location *string `json:"location,omitempty"`
@@ -2800,6 +2837,9 @@ type Resource struct {
 // MarshalJSON is the custom marshaler for Resource.
 func (r Resource) MarshalJSON() ([]byte, error) {
 	objectMap := make(map[string]interface{})
+	if r.Identity != nil {
+		objectMap["identity"] = r.Identity
+	}
 	if r.Location != nil {
 		objectMap["location"] = r.Location
 	}
@@ -3324,7 +3364,7 @@ type Workspace struct {
 	ID *string `json:"id,omitempty"`
 	// Name - READ-ONLY; Specifies the name of the resource.
 	Name *string `json:"name,omitempty"`
-	// Identity - READ-ONLY; The identity of the resource.
+	// Identity - The identity of the resource.
 	Identity *Identity `json:"identity,omitempty"`
 	// Location - Specifies the location of the resource.
 	Location *string `json:"location,omitempty"`
@@ -3341,6 +3381,9 @@ func (w Workspace) MarshalJSON() ([]byte, error) {
 	objectMap := make(map[string]interface{})
 	if w.WorkspaceProperties != nil {
 		objectMap["properties"] = w.WorkspaceProperties
+	}
+	if w.Identity != nil {
+		objectMap["identity"] = w.Identity
 	}
 	if w.Location != nil {
 		objectMap["location"] = w.Location
@@ -3613,6 +3656,8 @@ type WorkspaceProperties struct {
 	Encryption *EncryptionProperty `json:"encryption,omitempty"`
 	// HbiWorkspace - The flag to signal HBI data in the workspace and reduce diagnostic data collected by the service
 	HbiWorkspace *bool `json:"hbiWorkspace,omitempty"`
+	// ServiceProvisionedResourceGroup - READ-ONLY; The name of the managed resource group created by workspace RP in customer subscription if the workspace is CMK workspace
+	ServiceProvisionedResourceGroup *string `json:"serviceProvisionedResourceGroup,omitempty"`
 }
 
 // WorkspacePropertiesUpdateParameters the parameters for updating the properties of a machine learning
