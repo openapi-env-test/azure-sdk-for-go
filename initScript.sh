64e5d0bb9290e13622d64a975f24f4d4a9a64b9d
Dapeng Zhang <dapzhang@microsoft.com>
2020-11-03 16:52:31 +0800

    Add dep ensure in init

--- initScript.sh
+++ initScript.sh
@@ -6,10 +6,11 @@ then
   mkdir $GOPATH/bin
 fi
 curl -sSL https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
+dep ensure
 cat > $2 << EOF
 {
   "envs": {
-    "PATH": "$PATH:$GOPATH/bin",
+    "PATH": "$PATH:$GOPATH/bin:$GOPATH",
     "GOPATH": "$GOPATH"
   }
 }
