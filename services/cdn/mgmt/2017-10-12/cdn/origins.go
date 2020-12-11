0ab72d2bf8b19f49573fd5ec76e044bf23ee53e8
SDKAuto <sdkautomation@microsoft.com>
2020-12-11 02:20:00 +0000

    CodeGen from PR 898 in openapi-env-test/azure-rest-api-specs Merge 6b63b0bbebdc6903f753ab7a671ef7f725623acf into 78fb3bcd3025011cf047837f828a23c5ef242bef

--- services/cdn/mgmt/2017-10-12/cdn/origins.go
+++ services/cdn/mgmt/2017-10-12/cdn/origins.go
@@ -83,6 +83,7 @@ func (client OriginsClient) Get(ctx context.Context, resourceGroupName string, p
 	result, err = client.GetResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "cdn.OriginsClient", "Get", resp, "Failure responding to request")
+		return
 	}
 
 	return
@@ -170,9 +171,11 @@ func (client OriginsClient) ListByEndpoint(ctx context.Context, resourceGroupNam
 	result.olr, err = client.ListByEndpointResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "cdn.OriginsClient", "ListByEndpoint", resp, "Failure responding to request")
+		return
 	}
 	if result.olr.hasNextLink() && result.olr.IsEmpty() {
 		err = result.NextWithContext(ctx)
+		return
 	}
 
 	return
