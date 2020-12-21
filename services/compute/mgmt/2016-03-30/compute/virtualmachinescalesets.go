0816143895b0861ec83fd60fc49fa13068d0a950
SDKAuto <sdkautomation@microsoft.com>
2020-12-21 08:05:34 +0000

    CodeGen from PR 894 in openapi-env-test/azure-rest-api-specs Merge 893c96216c4da59415696db58d6967aeaea988b6 into db4a9ac9ae1fb323b38993d4605f91a2754bf39c

--- services/compute/mgmt/2016-03-30/compute/virtualmachinescalesets.go
+++ services/compute/mgmt/2016-03-30/compute/virtualmachinescalesets.go
@@ -562,6 +562,7 @@ func (client VirtualMachineScaleSetsClient) List(ctx context.Context, resourceGr
 	}
 	if result.vmsslr.hasNextLink() && result.vmsslr.IsEmpty() {
 		err = result.NextWithContext(ctx)
+		return
 	}
 
 	return
@@ -622,7 +623,6 @@ func (client VirtualMachineScaleSetsClient) listNextResults(ctx context.Context,
 	result, err = client.ListResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "compute.VirtualMachineScaleSetsClient", "listNextResults", resp, "Failure responding to next results request")
-		return
 	}
 	return
 }
@@ -678,6 +678,7 @@ func (client VirtualMachineScaleSetsClient) ListAll(ctx context.Context) (result
 	}
 	if result.vmsslwlr.hasNextLink() && result.vmsslwlr.IsEmpty() {
 		err = result.NextWithContext(ctx)
+		return
 	}
 
 	return
@@ -737,7 +738,6 @@ func (client VirtualMachineScaleSetsClient) listAllNextResults(ctx context.Conte
 	result, err = client.ListAllResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "compute.VirtualMachineScaleSetsClient", "listAllNextResults", resp, "Failure responding to next results request")
-		return
 	}
 	return
 }
@@ -795,6 +795,7 @@ func (client VirtualMachineScaleSetsClient) ListSkus(ctx context.Context, resour
 	}
 	if result.vmsslsr.hasNextLink() && result.vmsslsr.IsEmpty() {
 		err = result.NextWithContext(ctx)
+		return
 	}
 
 	return
@@ -856,7 +857,6 @@ func (client VirtualMachineScaleSetsClient) listSkusNextResults(ctx context.Cont
 	result, err = client.ListSkusResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "compute.VirtualMachineScaleSetsClient", "listSkusNextResults", resp, "Failure responding to next results request")
-		return
 	}
 	return
 }
