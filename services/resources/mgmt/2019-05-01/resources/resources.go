fe8614aa50717f48de7751ce026bbd0a15572d4e
SDKAuto <sdkautomation@microsoft.com>
2020-12-25 08:45:44 +0000

    CodeGen from PR 906 in openapi-env-test/azure-rest-api-specs Merge 4d67d87011e866bb6500092e14c984ac2c1c3a66 into 2501c0903eb81b9057cb15b2f23b31236146d613

--- services/resources/mgmt/2019-05-01/resources/resources.go
+++ services/resources/mgmt/2019-05-01/resources/resources.go
@@ -761,6 +761,7 @@ func (client Client) List(ctx context.Context, filter string, expand string, top
 	}
 	if result.lr.hasNextLink() && result.lr.IsEmpty() {
 		err = result.NextWithContext(ctx)
+		return
 	}
 
 	return
@@ -829,7 +830,6 @@ func (client Client) listNextResults(ctx context.Context, lastResults ListResult
 	result, err = client.ListResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "resources.Client", "listNextResults", resp, "Failure responding to next results request")
-		return
 	}
 	return
 }
@@ -907,6 +907,7 @@ func (client Client) ListByResourceGroup(ctx context.Context, resourceGroupName 
 	}
 	if result.lr.hasNextLink() && result.lr.IsEmpty() {
 		err = result.NextWithContext(ctx)
+		return
 	}
 
 	return
@@ -976,7 +977,6 @@ func (client Client) listByResourceGroupNextResults(ctx context.Context, lastRes
 	result, err = client.ListByResourceGroupResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "resources.Client", "listByResourceGroupNextResults", resp, "Failure responding to next results request")
-		return
 	}
 	return
 }
