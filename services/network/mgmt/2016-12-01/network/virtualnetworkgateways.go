443bfc32068fb721548140dfcca378b5449a9237
SDKAuto <sdkautomation@microsoft.com>
2020-12-25 08:57:48 +0000

    CodeGen from PR 906 in openapi-env-test/azure-rest-api-specs Merge 4d67d87011e866bb6500092e14c984ac2c1c3a66 into 2501c0903eb81b9057cb15b2f23b31236146d613

--- services/network/mgmt/2016-12-01/network/virtualnetworkgateways.go
+++ services/network/mgmt/2016-12-01/network/virtualnetworkgateways.go
@@ -624,6 +624,7 @@ func (client VirtualNetworkGatewaysClient) List(ctx context.Context, resourceGro
 	}
 	if result.vnglr.hasNextLink() && result.vnglr.IsEmpty() {
 		err = result.NextWithContext(ctx)
+		return
 	}
 
 	return
@@ -684,7 +685,6 @@ func (client VirtualNetworkGatewaysClient) listNextResults(ctx context.Context, 
 	result, err = client.ListResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "network.VirtualNetworkGatewaysClient", "listNextResults", resp, "Failure responding to next results request")
-		return
 	}
 	return
 }
