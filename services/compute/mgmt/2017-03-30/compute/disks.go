5cd211dcaa91fe6259ce870f791dc59faa025aa5
SDKAuto <sdkautomation@microsoft.com>
2020-12-31 09:42:29 +0000

    CodeGen from PR 911 in openapi-env-test/azure-rest-api-specs Merge 6be8b425c01c5656454211f3ff56fe1d56a4fe2a into 3cdbbde53a369d06c4c97f79587d93d1206bb416

--- services/compute/mgmt/2017-03-30/compute/disks.go
+++ services/compute/mgmt/2017-03-30/compute/disks.go
@@ -418,6 +418,7 @@ func (client DisksClient) List(ctx context.Context) (result DiskListPage, err er
 	}
 	if result.dl.hasNextLink() && result.dl.IsEmpty() {
 		err = result.NextWithContext(ctx)
+		return
 	}
 
 	return
@@ -477,7 +478,6 @@ func (client DisksClient) listNextResults(ctx context.Context, lastResults DiskL
 	result, err = client.ListResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "compute.DisksClient", "listNextResults", resp, "Failure responding to next results request")
-		return
 	}
 	return
 }
@@ -533,6 +533,7 @@ func (client DisksClient) ListByResourceGroup(ctx context.Context, resourceGroup
 	}
 	if result.dl.hasNextLink() && result.dl.IsEmpty() {
 		err = result.NextWithContext(ctx)
+		return
 	}
 
 	return
@@ -593,7 +594,6 @@ func (client DisksClient) listByResourceGroupNextResults(ctx context.Context, la
 	result, err = client.ListByResourceGroupResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "compute.DisksClient", "listByResourceGroupNextResults", resp, "Failure responding to next results request")
-		return
 	}
 	return
 }
