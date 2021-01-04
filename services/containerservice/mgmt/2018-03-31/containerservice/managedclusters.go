dc86f4fdb684a16f95a8b435d1c679d04738ed89
SDKAuto <sdkautomation@microsoft.com>
2021-01-04 09:40:12 +0000

    CodeGen from PR 862 in openapi-env-test/azure-rest-api-specs Merge 60340551ff4127f864fecc3115f300311cff1d59 into 92ca5032b81f43fa81f3157600988937d94501ae

--- services/containerservice/mgmt/2018-03-31/containerservice/managedclusters.go
+++ services/containerservice/mgmt/2018-03-31/containerservice/managedclusters.go
@@ -516,6 +516,7 @@ func (client ManagedClustersClient) List(ctx context.Context) (result ManagedClu
 	}
 	if result.mclr.hasNextLink() && result.mclr.IsEmpty() {
 		err = result.NextWithContext(ctx)
+		return
 	}
 
 	return
@@ -575,7 +576,6 @@ func (client ManagedClustersClient) listNextResults(ctx context.Context, lastRes
 	result, err = client.ListResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "containerservice.ManagedClustersClient", "listNextResults", resp, "Failure responding to next results request")
-		return
 	}
 	return
 }
@@ -638,6 +638,7 @@ func (client ManagedClustersClient) ListByResourceGroup(ctx context.Context, res
 	}
 	if result.mclr.hasNextLink() && result.mclr.IsEmpty() {
 		err = result.NextWithContext(ctx)
+		return
 	}
 
 	return
@@ -698,7 +699,6 @@ func (client ManagedClustersClient) listByResourceGroupNextResults(ctx context.C
 	result, err = client.ListByResourceGroupResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "containerservice.ManagedClustersClient", "listByResourceGroupNextResults", resp, "Failure responding to next results request")
-		return
 	}
 	return
 }
