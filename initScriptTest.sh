195fef9bfaf75e0e6f0f573b965608225526781e
tadelesh <tadelesh.shi@live.cn>
2021-08-13 14:54:42 +0800

    remove dep ensure

--- initScriptTest.sh
+++ initScriptTest.sh
@@ -5,9 +5,7 @@ if [ ! -d "$GOPATH/bin" ]
 then
   mkdir $GOPATH/bin
 fi
-curl -sSL https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
 PATH=$PATH:$GOPATH/bin
-dep ensure
 export GO111MODULE=on
 go get github.com/openapi-env-test/azure-sdk-for-go/tools/generator@track2_automation_and_release
 cat > $2 << EOF
