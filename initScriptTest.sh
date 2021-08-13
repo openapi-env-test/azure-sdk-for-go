5d0f73d2de7232efd971660a8ca7502616dd24b1
tadelesh <tadelesh.shi@live.cn>
2021-08-13 17:36:40 +0800

    test

--- initScriptTest.sh
+++ initScriptTest.sh
@@ -10,6 +10,7 @@ export GO111MODULE=on
 cd tools/generator && go build && cp generator $GOPATH/bin && cd ../..
 ln -s /usr/bin/pwsh $GOPATH/bin/pwsh.exe
 pwsh -Command Get-ChildItem -recurse -path ./sdk -filter go.mod
+ls sdk
 cat > $2 << EOF
 {
   "envs": {
