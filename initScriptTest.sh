f19f308c9b22be34350889a91a733202c7f3d22f
tadelesh <tadelesh.shi@live.cn>
2021-08-13 15:49:14 +0800

    try to fix pwsh

--- initScriptTest.sh
+++ initScriptTest.sh
@@ -8,7 +8,7 @@ fi
 PATH=$PATH:$GOPATH/bin
 export GO111MODULE=on
 cd tools/generator && go build && cp generator $GOPATH/bin && cd ../..
-whereis pwsh
+ln -s /usr/bin/pwsh /usr/bin/pwsh.exe
 cat > $2 << EOF
 {
   "envs": {
