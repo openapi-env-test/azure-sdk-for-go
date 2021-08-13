cd4a25a631ff66f5afb5492bf2a5e37330267dcb
tadelesh <tadelesh.shi@live.cn>
2021-08-13 15:11:37 +0800

    cp build file to path

--- initScriptTest.sh
+++ initScriptTest.sh
@@ -7,9 +7,7 @@ then
 fi
 PATH=$PATH:$GOPATH/bin
 export GO111MODULE=on
-cd tools/generator
-go build
-ls
+cd tools/generator && go build && cp generator $GOPATH/bin && cd ../..
 cat > $2 << EOF
 {
   "envs": {
