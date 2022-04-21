256d39d2bf6236a663660eae1fdc404f470fa0ff
SDKAuto <sdkautomation@microsoft.com>
2022-04-21 05:33:01 +0000

    CodeGen from PR 3102 in openapi-env-test/azure-rest-api-specs Merge 5332c5f7fadb59c1c37c895249043c4df37b82e3 into dd528db889505ce821d95459404aba16870aef24

--- services/preview/confidentialledger/mgmt/2021-05-13-preview/confidentialledger/models.go
+++ services/preview/confidentialledger/mgmt/2021-05-13-preview/confidentialledger/models.go
@@ -18,7 +18,7 @@ import (
 )
 
 // The package's fully qualified name.
-const fqdn = "github.com/Azure/azure-sdk-for-go/services/preview/confidentialledger/mgmt/2020-12-01-preview/confidentialledger"
+const fqdn = "github.com/Azure/azure-sdk-for-go/services/preview/confidentialledger/mgmt/2021-05-13-preview/confidentialledger"
 
 // AADBasedSecurityPrincipal AAD based security principal with associated Ledger RoleName
 type AADBasedSecurityPrincipal struct {
@@ -26,7 +26,7 @@ type AADBasedSecurityPrincipal struct {
 	PrincipalID *string `json:"principalId,omitempty"`
 	// TenantID - UUID/GUID based Tenant Id of the Security Principal
 	TenantID *string `json:"tenantId,omitempty"`
-	// LedgerRoleName - Possible values include: 'LedgerRoleNameReader', 'LedgerRoleNameContributor', 'LedgerRoleNameAdministrator'
+	// LedgerRoleName - Possible values include: 'Reader', 'Contributor', 'Administrator'
 	LedgerRoleName LedgerRoleName `json:"ledgerRoleName,omitempty"`
 }
 
@@ -34,8 +34,27 @@ type AADBasedSecurityPrincipal struct {
 type CertBasedSecurityPrincipal struct {
 	// Cert - Public key of the user cert (.pem or .cer)
 	Cert *string `json:"cert,omitempty"`
-	// LedgerRoleName - Possible values include: 'LedgerRoleNameReader', 'LedgerRoleNameContributor', 'LedgerRoleNameAdministrator'
+	// LedgerRoleName - Possible values include: 'Reader', 'Contributor', 'Administrator'
 	LedgerRoleName LedgerRoleName `json:"ledgerRoleName,omitempty"`
+}
+
+// CheckNameAvailabilityRequest the check availability request body.
+type CheckNameAvailabilityRequest struct {
+	// Name - The name of the resource for which availability needs to be checked.
+	Name *string `json:"name,omitempty"`
+	// Type - The resource type.
+	Type *string `json:"type,omitempty"`
+}
+
+// CheckNameAvailabilityResponse the check availability result.
+type CheckNameAvailabilityResponse struct {
+	autorest.Response `json:"-"`
+	// NameAvailable - Indicates if the resource name is available.
+	NameAvailable *bool `json:"nameAvailable,omitempty"`
+	// Reason - The reason why the given name is not available. Possible values include: 'Invalid', 'AlreadyExists'
+	Reason CheckNameAvailabilityReason `json:"reason,omitempty"`
+	// Message - Detailed reason why the given name is available.
+	Message *string `json:"message,omitempty"`
 }
 
 // ErrorAdditionalInfo the resource management error additional info.
@@ -167,9 +186,7 @@ type LedgerProperties struct {
 	IdentityServiceURI *string `json:"identityServiceUri,omitempty"`
 	// LedgerInternalNamespace - READ-ONLY; Internal namespace for the Ledger
 	LedgerInternalNamespace *string `json:"ledgerInternalNamespace,omitempty"`
-	// LedgerStorageAccount - Name of the Blob Storage Account for saving ledger files
-	LedgerStorageAccount *string `json:"ledgerStorageAccount,omitempty"`
-	// LedgerType - Type of Confidential Ledger. Possible values include: 'LedgerTypeUnknown', 'LedgerTypePublic', 'LedgerTypePrivate'
+	// LedgerType - Type of Confidential Ledger. Possible values include: 'Unknown', 'Public', 'Private'
 	LedgerType LedgerType `json:"ledgerType,omitempty"`
 	// ProvisioningState - READ-ONLY; Provisioning state of Ledger Resource. Possible values include: 'ProvisioningStateUnknown', 'ProvisioningStateSucceeded', 'ProvisioningStateFailed', 'ProvisioningStateCanceled', 'ProvisioningStateCreating', 'ProvisioningStateDeleting', 'ProvisioningStateUpdating'
 	ProvisioningState ProvisioningState `json:"provisioningState,omitempty"`
@@ -182,9 +199,6 @@ type LedgerProperties struct {
 // MarshalJSON is the custom marshaler for LedgerProperties.
 func (lp LedgerProperties) MarshalJSON() ([]byte, error) {
 	objectMap := make(map[string]interface{})
-	if lp.LedgerStorageAccount != nil {
-		objectMap["ledgerStorageAccount"] = lp.LedgerStorageAccount
-	}
 	if lp.LedgerType != "" {
 		objectMap["ledgerType"] = lp.LedgerType
 	}
@@ -195,48 +209,6 @@ func (lp LedgerProperties) MarshalJSON() ([]byte, error) {
 		objectMap["certBasedSecurityPrincipals"] = lp.CertBasedSecurityPrincipals
 	}
 	return json.Marshal(objectMap)
-}
-
-// LedgerUpdateFuture an abstraction for monitoring and retrieving the results of a long-running operation.
-type LedgerUpdateFuture struct {
-	azure.FutureAPI
-	// Result returns the result of the asynchronous operation.
-	// If the operation has not completed it will return an error.
-	Result func(LedgerClient) (Model, error)
-}
-
-// UnmarshalJSON is the custom unmarshaller for CreateFuture.
-func (future *LedgerUpdateFuture) UnmarshalJSON(body []byte) error {
-	var azFuture azure.Future
-	if err := json.Unmarshal(body, &azFuture); err != nil {
-		return err
-	}
-	future.FutureAPI = &azFuture
-	future.Result = future.result
-	return nil
-}
-
-// result is the default implementation for LedgerUpdateFuture.Result.
-func (future *LedgerUpdateFuture) result(client LedgerClient) (mVar Model, err error) {
-	var done bool
-	done, err = future.DoneWithContext(context.Background(), client)
-	if err != nil {
-		err = autorest.NewErrorWithError(err, "confidentialledger.LedgerUpdateFuture", "Result", future.Response(), "Polling failure")
-		return
-	}
-	if !done {
-		mVar.Response.Response = future.Response()
-		err = azure.NewAsyncOpIncompleteError("confidentialledger.LedgerUpdateFuture")
-		return
-	}
-	sender := autorest.DecorateSender(client, autorest.DoRetryForStatusCodes(client.RetryAttempts, client.RetryDuration, autorest.StatusCodesForRetry...))
-	if mVar.Response.Response, err = future.GetResult(sender); err == nil && mVar.Response.Response.StatusCode != http.StatusNoContent {
-		mVar, err = client.UpdateResponder(mVar.Response.Response)
-		if err != nil {
-			err = autorest.NewErrorWithError(err, "confidentialledger.LedgerUpdateFuture", "Result", mVar.Response.Response, "Failure responding to request")
-		}
-	}
-	return
 }
 
 // List object that includes an array of Confidential Ledgers and a possible link for next set.
@@ -398,12 +370,6 @@ func NewListPage(cur List, getNextPage func(context.Context, List) (List, error)
 	}
 }
 
-// Location location of the ARM Resource
-type Location struct {
-	// Location - The Azure location where the Confidential Ledger is running.
-	Location *string `json:"location,omitempty"`
-}
-
 // Model confidential Ledger. Contains the properties of Confidential Ledger Resource.
 type Model struct {
 	autorest.Response `json:"-"`
@@ -454,6 +420,12 @@ type Resource struct {
 func (r Resource) MarshalJSON() ([]byte, error) {
 	objectMap := make(map[string]interface{})
 	return json.Marshal(objectMap)
+}
+
+// ResourceLocation location of the ARM Resource
+type ResourceLocation struct {
+	// Location - The Azure location where the Confidential Ledger is running.
+	Location *string `json:"location,omitempty"`
 }
 
 // ResourceProviderOperationDefinition describes the Resource Provider Operation.
@@ -648,13 +620,13 @@ func NewResourceProviderOperationListPage(cur ResourceProviderOperationList, get
 type SystemData struct {
 	// CreatedBy - The identity that created the resource.
 	CreatedBy *string `json:"createdBy,omitempty"`
-	// CreatedByType - The type of identity that created the resource. Possible values include: 'CreatedByTypeUser', 'CreatedByTypeApplication', 'CreatedByTypeManagedIdentity', 'CreatedByTypeKey'
+	// CreatedByType - The type of identity that created the resource. Possible values include: 'User', 'Application', 'ManagedIdentity', 'Key'
 	CreatedByType CreatedByType `json:"createdByType,omitempty"`
 	// CreatedAt - The timestamp of resource creation (UTC).
 	CreatedAt *date.Time `json:"createdAt,omitempty"`
 	// LastModifiedBy - The identity that last modified the resource.
 	LastModifiedBy *string `json:"lastModifiedBy,omitempty"`
-	// LastModifiedByType - The type of identity that last modified the resource. Possible values include: 'CreatedByTypeUser', 'CreatedByTypeApplication', 'CreatedByTypeManagedIdentity', 'CreatedByTypeKey'
+	// LastModifiedByType - The type of identity that last modified the resource. Possible values include: 'User', 'Application', 'ManagedIdentity', 'Key'
 	LastModifiedByType CreatedByType `json:"lastModifiedByType,omitempty"`
 	// LastModifiedAt - The timestamp of resource last modification (UTC)
 	LastModifiedAt *date.Time `json:"lastModifiedAt,omitempty"`
