60fa4a50ba0e7fdb02850b27d40b1693cc8c3de9
SDKAuto <sdkautomation@microsoft.com>
2022-07-19 05:32:17 +0000

    CodeGen from PR 2525 in test-repo-billy/azure-rest-api-specs Merge fad0402dca7edb6c99b2a967eca1b1178fc426a0 into c1af2935f8aa5fd6f90139b1a96f8725f5795d63

--- services/appconfiguration/mgmt/2020-06-01/appconfiguration/configurationstores.go
+++ services/appconfiguration/mgmt/2020-06-01/appconfiguration/configurationstores.go
@@ -295,7 +295,7 @@ func (client ConfigurationStoresClient) GetResponder(resp *http.Response) (resul
 	return
 }
 
-// List lists the configuration stores for a given subscription.
+// List lists the configuration stores for a given subscriptionss.
 // Parameters:
 // skipToken - a skip token is used to continue retrieving items after an operation returns a partial result.
 // If a previous response contains a nextLink element, the value of the nextLink element will include a
